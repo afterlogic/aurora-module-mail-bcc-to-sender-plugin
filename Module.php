593dbced1eb5d1e911f7c80fbec1d68cd6b2ddba
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:56 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ namespace Aurora\Modules\MailBccToSenderPlugin;
 
 /**
  * With this plugin enabled and configured, all email messages sent out will also be delivered to a message sender, regardless of whether the sender's address was supplied in CC / BCC or not.
- * 
+ *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2023, Afterlogic Corp.
@@ -18,26 +18,26 @@ namespace Aurora\Modules\MailBccToSenderPlugin;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	/**
-	 * @var \Aurora\Modules\Mail\Module
-	 */
-	protected $oMailModule;
-	
-	public function init() 
-	{
-		$this->oMailModule = \Aurora\System\Api::GetModule('Mail');
-	
-		$this->subscribeEvent('Mail::SendMessage::before', array($this, 'onBeforeSendMessage'));
-	}
-	
-	/**
-	 * 
-	 * @param array $aArguments
-	 * @param mixed $mResult
-	 */
-	public function onBeforeSendMessage(&$aArguments, &$mResult)
-	{	
-		$oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccount($aArguments['AccountID']);
-		$aArguments['Bcc'] .= ($aArguments['Bcc']!==''?', ':'') . $oAccount->Email;
-	}
+    /**
+     * @var \Aurora\Modules\Mail\Module
+     */
+    protected $oMailModule;
+
+    public function init()
+    {
+        $this->oMailModule = \Aurora\System\Api::GetModule('Mail');
+
+        $this->subscribeEvent('Mail::SendMessage::before', array($this, 'onBeforeSendMessage'));
+    }
+
+    /**
+     *
+     * @param array $aArguments
+     * @param mixed $mResult
+     */
+    public function onBeforeSendMessage(&$aArguments, &$mResult)
+    {
+        $oAccount = \Aurora\Modules\Mail\Module::Decorator()->GetAccount($aArguments['AccountID']);
+        $aArguments['Bcc'] .= ($aArguments['Bcc']!=='' ? ', ' : '') . $oAccount->Email;
+    }
 }
